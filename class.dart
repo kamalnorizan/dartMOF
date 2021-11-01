3a71d1c2e2dbf4650b01a7b4f78a162595a58a0e
kamalnorizan <kamalnorizan@gmail.com>
2021-11-01 15:18:45 +0800

    List owner

--- class.dart
+++ class.dart
@@ -18,9 +18,9 @@ class Car{
   String? color;
   double? cc;
   double? price;
-  Owner owner;
+  List<Owner> owners;
 
-  Car(this.owner,{this.brand, 
+  Car(this.owners,{this.brand, 
        this.carseats, 
        this.color, 
        this.cc=1.5, 
@@ -32,23 +32,40 @@ class Car{
     print(color);
     print(cc);
     print(price);
-    owner.printOwner();
+    for(var i=0; i < owners.length; i++){
+      owners[i].printOwner();
+    }
+    print('==========================');
   }
 }
 
 void main() {
-  List<Car> cars = [Car(Owner(name: 'Kamal', age: 20, phoneNumber: '0123456789'),brand: 'Proton', carseats: 5, color: 'red'),
-              Car(Owner(name: 'Zain', age: 30, phoneNumber: '01234568889'), brand: 'Porche Carrera', carseats: 2, color: 'red', cc: 3.6, price: 400000)];
+  List<Car> cars = [
+    Car(
+      [Owner(name: 'Kamal', age: 20, phoneNumber: '0123456789'),
+      Owner(name: 'Shafiq', age: 22, phoneNumber: '0123343889'),
+      Owner(name: 'Azman', age: 21, phoneNumber: '0143343889')],
+      brand: 'Proton', 
+      carseats: 5, 
+      color: 'red'),
+    Car(
+      [Owner(name: 'Zain', age: 30, phoneNumber: '01234568889')], 
+      brand: 'Porche Carrera', 
+      carseats: 2, 
+      color: 'red', 
+      cc: 3.6, 
+      price: 400000)];
+  
   print(cars);
   print('For Loop');
   for(var i=0; i < cars.length; i++){
     cars[i].printCar();
   }
   
-  var num = 0;
-  print('While Loop');
-  while(num<cars.length){
-    cars[num].printCar();
-    num++;
-  }
+//   var num = 0;
+//   print('While Loop');
+//   while(num<cars.length){
+//     cars[num].printCar();
+//     num++;
+//   }
 }
